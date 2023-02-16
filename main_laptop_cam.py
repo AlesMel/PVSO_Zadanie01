9f47a02c2409990ffbf077cd8c0caf8ee37dcee4
JakubMihalik <xmihalikj1@stuba.sk>
2023-02-16 20:33:44 +0100

    Postprocessing  with fixed dimensions

--- main_laptop_cam.py
+++ main_laptop_cam.py
@@ -2,13 +2,29 @@ from time import sleep
 import cv2 as cv
 import numpy as np
 
-WIDTH = 400
-HEIGHT = 300
+WIDTH: int = 400
+HEIGHT: int = 400
 
 
 def setup_camera():
     cam = cv.VideoCapture(0)
     return cam
+
+
+def post_process_image(image: np.ndarray, kernel: np.ndarray,
+                       img_index_x: int = 1, img_index_y: int = 1):
+    if isinstance(image, np.ndarray):
+        height = image.shape[0]
+        width = image.shape[1]
+
+        # Calculate corresponding dimensions
+        processed_part = cv.filter2D(np.array(image[:int(height / 2), :int(width / 2)]), -1, kernel)
+        image[:int(height / 2), :int(width / 2)] = processed_part
+
+        cv.imshow("image", image)
+        cv.waitKey()
+        return True
+    return False
 
 
 def process_image(width, height):
@@ -25,18 +41,10 @@ cam = setup_camera()
 cur_index = 0
 image_data = []
 
-# Edge detection kernel
-kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-
 while cur_index < 4:
-    processed_image = process_image(400, 300)
+    processed_image = process_image(WIDTH, HEIGHT)
     pressed = cv.waitKey(1)
     if pressed == ord(' '):
-
-        if cur_index == 0:
-            # If it's first image apply kernel
-            processed_image = cv.filter2D(processed_image, -1, kernel)
-
         image_data.append(processed_image)
         # Apply kernel
         cv.imwrite("pekne_fotky_{0}.jpg".format(cur_index), processed_image)
@@ -45,14 +53,22 @@ while cur_index < 4:
         break
     cv.imshow("Image", processed_image)
 
-# check the shape
-print(np.array(image_data).shape)
-
 # now concat the data but reshape them first to be 2x2 grid
-result = concat_images(np.array(image_data).reshape(2, 2, 300, 400, 3))
+result = concat_images(np.array(image_data).reshape(2, 2, HEIGHT, WIDTH, 3))
 
 # save the image as mosaic.jpg
 cv.imwrite("mosaic.jpg", np.array(result))
+
+# Post process mosaic
+mosaic = cv.imread("mosaic.jpg")
+
+kernel = np.array([[-1, -1, -1],
+                   [-1,  8, -1],
+                   [-1, -1, -1]])
+post_process_image(mosaic, kernel)
+
+cv.imshow("image", mosaic)
+cv.waitKey()
 
 # After the loop release the cap object
 cam.release()
--- testing.py
+++ testing.py
@@ -1,7 +1,29 @@
 import numpy as np
+import cv2 as cv
 
-mosaic = np.empty((4, 3, 4, 4,))
-mosaic2 = mosaic.reshape(2, 2, 3, 4, 4)
-print(mosaic)
-print(mosaic.shape)
-print(mosaic2)
+
+# mosaic = np.empty((4, 3, 4, 4,))
+# mosaic2 = mosaic.reshape(2, 2, 3, 4, 4)
+# print(mosaic)
+# print(mosaic.shape)
+# print(mosaic2)
+
+# Test dimensions
+# def post_process_image(image: np.ndarray, kernel: np.ndarray,
+#                        img_index_x: int = 1, img_index_y: int = 1):
+#     if isinstance(image, np.ndarray):
+#         height = image.shape[0]
+#         width = image.shape[1]
+#
+#         # Calculate corresponding dimensions
+#         processed_part = cv.filter2D(np.array(image[:int(height / 2), :int(width / 2)]), -1, kernel)
+#         image[:int(height / 2), :int(width / 2)] = processed_part
+#
+#         cv.imshow("image", image)
+#         cv.waitKey()
+#
+#         return True
+#     return False
+
+
+post_process_image(cv.imread("mosaic.jpg"), np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]))
